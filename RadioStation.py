533ba17fec062c5e9eb50c4db3e88904fd1109de
JoshHutch1 <80630854+JoshHutch1@users.noreply.github.com>
2021-07-23 21:07:51 +0100

    Add files via upload

---
+++ RadioStation.py
@@ -0,0 +1,230 @@
+import webbrowser
+import time
+
+#Welcome user
+name = input("What is your name? ")
+time.sleep(0.5)
+print("Hello", name,"What radio station do you want to listen to? ")
+station = input("") 
+
+#================================
+#=============Radio 1============
+#================================
+if station == "radio 1" or station == "Radio 1" or station == "bbc radio 1" or station == "BBC Radio 1" or station == "BBC radio 1":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.bbc.co.uk/sounds/play/live:bbc_radio_one')
+
+#================================
+#=============Radio 2============
+#================================
+elif station == "radio 2" or station == "Radio 2" or station == "bbc radio 2" or station == "BBC Radio 2" or station == "BBC radio 2":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.bbc.co.uk/sounds/play/live:bbc_radio_two')
+
+#================================
+#=============Radio 3============
+#================================
+elif station == "radio 3" or station == "Radio 3" or station == "bbc radio 3" or station == "BBC Radio 3" or station == "BBC radio 3":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.bbc.co.uk/sounds/play/live:bbc_radio_three')
+
+#================================
+#=============Radio 4============
+#================================
+elif station == "radio 4" or station == "Radio 4" or station == "bbc radio 4" or station == "BBC Radio 4" or station == "BBC radio 4":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.bbc.co.uk/sounds/play/live:bbc_radio_fourfm')
+
+#================================
+#==========Radio 5 live=========
+#================================
+elif station == "radio 5 live" or station == "Radio 5 live" or station == "bbc radio 5 live" or station == "BBC Radio 5 live" or station == "BBC radio 5 live":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.bbc.co.uk/sounds/play/live:bbc_radio_five_live')
+
+#================================
+#=========asian network==========
+#================================
+elif station == "Asian Network" or station == "asian Network" or station == "asian network" or station == "Asian Net Work" or station == "asian net work":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.bbc.co.uk/sounds/play/live:bbc_asian_network')
+
+#================================
+#==========Heart================
+#================================
+elif station == "Heart" or station == "heart":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.globalplayer.com/live/heart/uk/')
+
+#================================
+#=========Heart 70s==============
+#================================
+elif station == "Heart 70" or station == "Heart 70s" or station == "heart 70" or station == "heart 70s":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.globalplayer.com/live/heart70s/uk/')
+
+#================================
+#=========Heart 80s==============
+#================================
+elif station == "Heart 80" or station == "Heart 80s" or station == "heart 80" or station == "heart 80s":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.globalplayer.com/live/heart80s/uk/')
+
+#================================
+#=========Heart 90s==============
+#================================
+elif station == "Heart 90" or station == "Heart 90s" or station == "heart 90" or station == "heart 90s":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.globalplayer.com/live/heart90s/uk/')
+
+
+#================================
+#=========Heart Dance============
+#================================
+elif station == "Heart Dance" or station == "Heart dance" or station == "heart dance" or station == "heart Dance":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.globalplayer.com/live/heartdance/uk')
+
+#================================
+#=========Capital FM=============
+#================================
+elif station == "Capital FM" or station == "Capital Fm" or station == "capital FM" or station == "capital fm"  or station == "Capital fm":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.globalplayer.com/live/capital/uk')
+
+#================================
+#=========Classical FM===========
+#================================
+elif station == "Classical FM" or station == "Classical Fm" or station == "classical FM" or station == "classical fm"  or station == "classical fm":
+    time.sleep(1)
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(2)
+    print("Found Station!")
+    webbrowser.open('https://www.globalplayer.com/live/capital/uk')
+
+else:
+    print("Finding station")
+    time.sleep(1)
+    print("Finding station.")
+    time.sleep(1)
+    print("Finding station..")
+    time.sleep(1)
+    print("Finding station...")
+    time.sleep(1)
+    print('Sorry we cannot find radio station. We will search the web for you!')
+    time.sleep(2)
+    webbrowser.open("https://google.com/search?q=%s" % station)
